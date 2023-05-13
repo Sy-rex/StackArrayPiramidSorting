4cdfe8b6682d41882648ee0454247b5dde7c7758
sy-rex <sobolevkirill0@gmail.com>
2023-05-13 00:07:11 -0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackArrayPiramidSorting
+{
+    internal class Program
+    {
+        public class StackArray
+        {
+
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
